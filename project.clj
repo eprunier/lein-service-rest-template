fdad6d96ec91b545b49966ec169de59995545def
Eric Prunier <eric.prunier@gmail.com>
2014-07-12 15:18:57 +0200

    Rename project to lein-service-rest-template

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-api-rest-template/lein-template "1.0.0-SNAPSHOT"
-  :description "A Leiningen template for REST API based on Liberator"
+(defproject lein-service-rest-template/lein-template "1.0.0"
+  :description "A Leiningen template for REST services based on Liberator"
   :url "https://github.com/eprunier/lein-api-rest-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
